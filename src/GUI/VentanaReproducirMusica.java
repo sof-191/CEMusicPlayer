224524650dd38458007442361e9fb798735470e8
sof-191 <102682975+sof-191@users.noreply.github.com>
2022-09-22 00:39:25 -0600

    Fixes

--- src/GUI/VentanaReproducirMusica.java
+++ src/GUI/VentanaReproducirMusica.java
@@ -3,15 +3,13 @@ package GUI;
 import Clases.Bibliotecas.Bibliotecas;
 import Clases.Canciones.ListaDobleEnlazada;
 import Clases.Canciones.Nodo;
-import Clases.Canciones.ReproducirMusica;
 import Clases.Usuarios.ListaSimple;
 import Clases.Usuarios.Usuarios;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 import javax.swing.*;
-
-import javafx.embed.swing.JFXPanel;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.Media;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -27,42 +25,64 @@ public class VentanaReproducirMusica {
     private JButton bajarVolumenButton;
     private JButton subirVolumenButton;
     private JPanel jPanel;
+    private JButton regresarButton;
+    private JButton favoritaButton;
     private JFrame jFrame;
     private Usuarios usuario;
     private Clases.Usuarios.ListaSimple listaUsuarios;
     private Bibliotecas biblioteca;
-    private MediaPlayer mediaPlayer;
     private Nodo inicio, temp;
-    private ReproducirMusica reproducirMusica;
+    private JFXPanel fxPanel = new JFXPanel();
+    private static MediaPlayer mediaPlayer;
+
+    private boolean continua = false;
 
     public VentanaReproducirMusica(Usuarios usuario, ListaSimple listaUsuarios, Bibliotecas biblioteca) {
         this.usuario = usuario;
         this.listaUsuarios = listaUsuarios;
         this.biblioteca = biblioteca;
         initComponents();
-        reproducirMusica.Reproducir();
+        Reproducir();
         reproducirButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                reproducirMusica.Reproducir();
+                Reproducir();
             }
         });
         pausarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                reproducirMusica.Pausar();
+                Pausar();
             }
         });
         anteriorButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                reproducirMusica.Anterior();
+                Anterior();
+                textField1.setText(getCancion());
+
             }
         });
         siguienteButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                reproducirMusica.Siguiente();
+                Siguiente();
+                textField1.setText(getCancion());
+
+            }
+        });
+        regresarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                VentanaBibliotecas ventanaBibliotecas = new VentanaBibliotecas(listaUsuarios, usuario.getCorreoElectronico());
+                jFrame.setVisible(false);
+                mediaPlayer.stop();
+            }
+        });
+        loopButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cambiarContinua();
             }
         });
     }
@@ -74,13 +94,53 @@ public class VentanaReproducirMusica {
         jFrame.pack();
         jFrame.setVisible(true);
         ListaDobleEnlazada listaDobleEnlazada = biblioteca.getListaDeCanciones();
-        inicio = listaDobleEnlazada.getStart();
-        temp = listaDobleEnlazada.getStart();
-        reproducirMusica = new ReproducirMusica(biblioteca);
+        this.inicio = biblioteca.getListaDeCanciones().getStart();
+        this.temp = biblioteca.getListaDeCanciones().getStart();
+        cargarBiblioteca();
+        textField1.setText(getCancion());
 
 
     }
 
+    private void cargarBiblioteca() {
 
+        String path = "Canciones/" + temp.getData().getPath();
+        File file = new File(path);
+        Media media = new Media(file.toURI().toString());
+
+        this.mediaPlayer = new MediaPlayer(media);
+
+        mediaPlayer.setOnEndOfMedia(() -> {
+            if (temp.getNext() != inicio || continua) {
+                textField1.setText(getCancion());
+                temp = temp.getNext();
+                cargarBiblioteca();
+            }
+        });
+        Reproducir();
+
+    }
+    public void Reproducir(){
+        mediaPlayer.play();
+    }
+    public void Siguiente(){
+        mediaPlayer.stop();
+        temp = temp.getNext();
+        cargarBiblioteca();
+    }
+    public void Anterior(){
+        mediaPlayer.stop();
+        temp = temp.getPrev();
+        cargarBiblioteca();
+    }
+    public void Pausar(){
+        mediaPlayer.pause();
+    }
+    public void cambiarContinua(){
+        this.continua = !continua;
+    }
+    public String getCancion(){
+        return temp.getData().getNombre();
+    }
 
 }
